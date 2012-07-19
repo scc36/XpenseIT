5231dca72e56fa6fb5eadaa8065b7ed7a6a389a7
Shawn Chen <scc36c@gmail.com>
2012-07-19 01:08:11 -0400

    Initial Project Creation

---
+++ XpenseIT/XpenseITTests/XpenseITTests.h
@@ -0,0 +1,13 @@
+//
+//  XpenseITTests.h
+//  XpenseITTests
+//
+//  Created by briansj on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface XpenseITTests : SenTestCase
+
+@end
---
+++ XpenseIT/XpenseITTests/XpenseITTests.m
@@ -0,0 +1,32 @@
+//
+//  XpenseITTests.m
+//  XpenseITTests
+//
+//  Created by briansj on 7/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XpenseITTests.h"
+
+@implementation XpenseITTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in XpenseITTests");
+}
+
+@end
